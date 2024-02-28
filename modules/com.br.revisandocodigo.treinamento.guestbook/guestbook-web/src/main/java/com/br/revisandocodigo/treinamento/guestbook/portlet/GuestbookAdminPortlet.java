86085582dd2848fcd353644a90fc4f2b58737b5c
wesleyroberts <wesleyrobertsdesouza@gmail.com>
2024-02-28 16:34:36 -0300

    Adiciona arquivos jsp

---
+++ modules/com.br.revisandocodigo.treinamento.guestbook/guestbook-web/src/main/java/com/br/revisandocodigo/treinamento/guestbook/portlet/GuestbookAdminPortlet.java
@@ -0,0 +1,27 @@
+package com.br.revisandocodigo.treinamento.guestbook.portlet;
+
+import com.br.revisandocodigo.treinamento.guestbook.constants.GuestbookWebPortletKeys;
+import org.osgi.service.component.annotations.Component;
+
+import javax.portlet.Portlet;
+
+@Component(
+        immediate = true,
+        property = {
+                "com.liferay.portlet.display-category=category.hidden",
+                "com.liferay.portlet.scopeable=true",
+                "javax.portlet.display-name=Guestbooks",
+                "javax.portlet.expiration-cache=0",
+                "javax.portlet.init-param.portlet-title-based-navigation=true",
+                "javax.portlet.init-param.template-path=/",
+                "javax.portlet.init-param.view-template=/guestbook_admin/view.jsp",
+                "javax.portlet.name=" + GuestbookWebPortletKeys.GUESTBOOK_ADMIN,
+                "javax.portlet.resource-bundle=content.Language",
+                "javax.portlet.security-role-ref=administrator",
+                "javax.portlet.supports.mime-type=text/html",
+                "com.liferay.portlet.add-default-resource=true"
+        },
+        service = Portlet.class
+)
+public class GuestbookAdminPortlet {
+}
