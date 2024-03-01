ab022630ee395ae0ecd8a81c3e6d44a186e55e74
wesleyroberts <wesleyrobertsdesouza@gmail.com>
2024-03-01 17:08:32 -0300

    Adiciona Resource Permission na camada service

---
+++ modules/guestbook/guestbook-api/src/main/java/com/br/revisandocodigo/treinamento/guestbook/constants/GuestbookConstants.java
@@ -0,0 +1,6 @@
+package com.br.revisandocodigo.treinamento.guestbook.constants;
+
+public class GuestbookConstants {
+    public static final String RESOURCE_NAME = "com.br.revisandocodigo.treinamento.guestbook";
+
+}
